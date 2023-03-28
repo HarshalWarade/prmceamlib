0dcf52671f31a1b646eef2d53987cb98c2b34ef1
harshalDev <harshalwarade344@gmail.com>
2023-03-28 21:55:28 +0530

    changes made

---
+++ controllersJS.js
@@ -0,0 +1,7 @@
+let avam = document.getElementById('avam'); // l vission and mission
+let alc = document.getElementById('alc'); // l library committiee
+let afp = document.getElementById('afp'); // l faculty publication
+let alco = document.getElementById('alco');  
+let aoh = document.getElementById('aoh');
+let arar = document.getElementById('arar');
+let alt = document.getElementById('alt');
