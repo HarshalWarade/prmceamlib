54ab357a7e4827768fe95fcfedfeeed79b370dee
harshalDev <harshalwarade344@gmail.com>
2023-05-16 22:17:22 +0530

    images aligned properly

---
+++ footer.js
@@ -0,0 +1,68 @@
+document.write(
+    `<footer class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
+    <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary">
+      <div class="row">
+        <div class="col-lg-3 mb-3">
+          <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="/"
+            aria-label="Bootstrap">
+            <span class="fs-5">PRMCEAM LIBRARY</span>
+          </a>
+          <hr>
+          <ul class="list-unstyled small">
+            <li class="mb-2" style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dolorum eos, aut velit magni minus tenetur odit adipisci mollitia animi! Impedit officia voluptatibus
+              laborum animi numquam distinctio iusto dolorum magni sapiente.</li>
+          </ul>
+        </div>
+        <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+          <h5>Links</h5>
+          <hr>
+          <ul class="list-unstyled">
+            <li class="mb-2"><a href="/">Links</a></li>
+            <li class="mb-2"><a href="/">Links</a></li>
+            <li class="mb-2"><a href="/">Links</a></li>
+            <li class="mb-2"><a href="/">Links</a></li>
+            <li class="mb-2"><a href="/">Links</a></li>
+            <li class="mb-2"><a href="/">Links</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-3">
+          <h5>Something</h5>
+          <hr>
+          <ul class="list-unstyled">
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+            <li class="mb-2"><a href="/">Some Other Links</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-3">
+          <h5>Projects</h5>
+          <hr>
+          <ul class="list-unstyled">
+            <li class="mb-2"><a href="/">Other Links</a></li>
+            <li class="mb-2"><a href="/">Other Links</a></li>
+            <li class="mb-2"><a href="/">Other Links</a></li>
+            <li class="mb-2"><a href="/">Other Links</a></li>
+            <li class="mb-2"><a href="/">Other Links</a></li>
+            <li class="mb-2"><a href="/">Other Links</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-3">
+          <h5>Else Something</h5>
+          <hr>
+          <ul class="list-unstyled">
+            <li class="mb-2"><a href="/">Else links</a></li>
+            <li class="mb-2"><a href="/">Else links</a></li>
+            <li class="mb-2"><a href="/">Else links</a></li>
+            <li class="mb-2"><a href="/">Else links</a></li>
+            <li class="mb-2"><a href="/">Else links</a></li>
+            <li class="mb-2"><a href="/">Else links</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>`
+);
