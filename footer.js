26ce2009d02905f405df0efa72bf1dbd7d314a3f
harshalDev <harshalwarade344@gmail.com>
2023-06-11 14:53:46 +0530

    footer

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 document.write(
-    `<footer class="bd-footer py-4 py-md-5 mt-5 bg-body-tertiary">
-    <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary">
+    `<footer class="bd-footer py-2 py-md-5 mt-5 bg-body-tertiary">
+    <div class="container py-md-5 px-4 px-md-3 text-body-secondary">
       <div class="row">
         <div class="col-lg-3 mb-3">
           <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="/"
@@ -13,51 +13,51 @@ document.write(
           </ul>
         </div>
         <div class="col-6 col-lg-2 offset-lg-1 mb-3">
-          <h5>Links</h5>
-          <hr>
-          <ul class="list-unstyled">
+        <h5>QuickLinks</h5>
+        <hr>
+        <ul class="list-unstyled">
+        <li class="mb-2"><a href="index.html">HOME</a></li>
+        <li class="mb-2"><a href="qrcodes.html" target = "__blank">QRCodes</a></li>
+        <li class="mb-2"><a href="downloads.html" target = "__blank">Downloads</a></li>
             <li class="mb-2"><a href="https://docs.google.com/forms/d/e/1FAIpQLSckVzdg3A8t4fmZdP33tw8dr4q5Nr8G76GyOYCLaxzPsYrvXA/viewform" target = "__blank">SGBAU PYQs</a></li>
-            <li class="mb-2"><a href="https://sgbau.ac.in/pages/EmploymentBulletin.aspx" target = "__blank">Career/Employment Bulletin(SGBAU)</a></li>
-            <li class="mb-2"><a href="qrcodes.html" target = "__blank">QRCodes</a></li>
-            <li class="mb-2"><a href="downloads.html" target = "__blank">Downloads</a></li>
             <li class="mb-2"><a href="events.html" target = "__blank">Events and Activities</a></li>
-            <li class="mb-2"><a href="index.html">HOME</a></li>
+            <li class="mb-2"><a href="https://sgbau.ac.in/pages/EmploymentBulletin.aspx" target = "__blank">Career/Employment Bulletin(SGBAU)</a></li>
+          </ul>
+          </div>
+          <div class="col-6 col-lg-2 mb-3">
+          <h5>...</h5>
+          <hr>
+          <ul class="list-unstyled">
+            <li class="mb-2"><a href="gate.html">GATE</a></li>
+            <li class="mb-2"><a href="opac.html">OPAC</a></li>
+            <li class="mb-2"><a href="facultypublication.html">Faculty Publications</a></li>
+            <li class="mb-2"><a href="newsclips.html">Newspaper clippings</a></li>
+            <li class="mb-2"><a href="rulesandreg.html">Rules and Regulations</a></li>
+            <li class="mb-2"><a href="refmansoft.html">Reference Management Softwares</a></li>
           </ul>
         </div>
         <div class="col-6 col-lg-2 mb-3">
-          <h5>Something</h5>
-          <hr>
+        <h5>...</h5>
+        <hr>
           <ul class="list-unstyled">
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Some Other Links</a></li>
+          <li class="mb-2"><a href="events.html">Events</a></li>
+          <li class="mb-2"><a href="delnet.html">DELNET</a></li>
+            <li class="mb-2"><a href="genServices.html">General Services</a></li>
+            <li class="mb-2"><a href="docdelser.html">Document Delivery Services</a></li>
+            <li class="mb-2"><a href="index.html">Institutional Repository</a></li>
+            <li class="mb-2"><a href="perresguide.html">Personalised Research Id's</a></li>
           </ul>
         </div>
         <div class="col-6 col-lg-2 mb-3">
-          <h5>Projects</h5>
+          <h5>...</h5>
           <hr>
           <ul class="list-unstyled">
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-            <li class="mb-2"><a href="index.html">Other Links</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-lg-2 mb-3">
-          <h5>Else Something</h5>
-          <hr>
-          <ul class="list-unstyled">
-            <li class="mb-2"><a href="index.html">Else links</a></li>
-            <li class="mb-2"><a href="index.html">Else links</a></li>
-            <li class="mb-2"><a href="index.html">Else links</a></li>
-            <li class="mb-2"><a href="index.html">Else links</a></li>
-            <li class="mb-2"><a href="index.html">Else links</a></li>
-            <li class="mb-2"><a href="index.html">Else links</a></li>
+          <li class="mb-2"><a href="ndl.html">NDL</a></li>
+          <li class="mb-2"><a href="faqs.html">FAQs</a></li>
+          <li class="mb-2"><a href="nlist.html">NLIST</a></li>
+            <li class="mb-2"><a href="libraryteam.html">Library Team</a></li>
+            <li class="mb-2"><a href="libcomittee.html">Library Committee</a></li>
+            <li class="mb-2"><a href="ndloic.html">National Digital Library Of India Club</a></li>
           </ul>
         </div>
       </div>
