0ba4483bfabc23bec3ab38b823e4f7b3554d3f61
harshalDev <harshalwarade344@gmail.com>
2023-08-02 17:14:32 +0530

    last updated 02 - 08 - 2023

--- footer.js
+++ footer.js
@@ -1,68 +1,82 @@
 document.write(
   `   
-    <footer class="bd-footer py-2 py-md-5 mt-5 bg-body-tertiary">
-    <div class="container py-md-5 px-4 px-md-3 text-body-secondary">
-      <div class="row">
-        <div class="col-lg-3 mb-3">
-          <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="/"
-            aria-label="Bootstrap">
-            <span class="fs-5">PRMCEAM LIBRARY</span>
-          </a>
-          <hr>
-          <ul class="list-unstyled small">
-            <li class="mb-2" style="text-align: justify;">Central Library is a seat of learning endeavor in any institution and so is ours. Central Library believes in the “Student first” philosophy and gives priority to serving students first.  Keeping pace with growing resources in the world, we strive hard to achieve the expectations of users and make them technologically sound by making available global resources all in one place.  PRMCEAM Library is on its way to becoming an outstanding learning resource center catering to the ever-growing and uncompromising information and intellectual requirements of the students and faculty.</li>
-          </ul>
-        </div>
-        <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+  <footer class="bd-footer py-2 py-md-5 mt-5 bg-body-tertiary">
+  <div class="container py-md-5 px-4 px-md-3 text-body-secondary">
+    <div class="row">
+      <div class="col-lg-3 mb-3">
+        <a class="d-inline-flex align-items-center mb-2 text-body-emphasis text-decoration-none" href="/"
+          aria-label="Bootstrap">
+          <span class="fs-5">PRMCEAM LIBRARY</span>
+        </a>
+        <hr>
+        <ul class="list-unstyled small">
+          <li class="mb-2" style="text-align: justify;">Central Library is a seat of learning endeavor in
+            any institution and so is ours. Central Library believes in the “Student first” philosophy
+            and gives priority to serving students first. Keeping pace with growing resources in the
+            world, we strive hard to achieve the expectations of users and make them technologically
+            sound by making available global resources all in one place. PRMCEAM Library is on its way
+            to becoming an outstanding learning resource center catering to the ever-growing and
+            uncompromising information and intellectual requirements of the students and faculty.</li>
+        </ul>
+      </div>
+      <div class="col-6 col-lg-2 offset-lg-1 mb-3">
         <h5>QuickLinks</h5>
         <hr>
         <ul class="list-unstyled">
-        <li class="mb-2"><a href="index.html">HOME</a></li>
-        <li class="mb-2"><a href="qrcodes.html" target = "__blank">QRCodes</a></li>
-        <li class="mb-2"><a href="downloads.html" target = "__blank">Downloads</a></li>
-            <li class="mb-2"><a href="https://docs.google.com/forms/d/e/1FAIpQLSckVzdg3A8t4fmZdP33tw8dr4q5Nr8G76GyOYCLaxzPsYrvXA/viewform" target = "__blank">SGBAU PYQs</a></li>
-            <li class="mb-2"><a href="events.html" target = "__blank">Events and Activities</a></li>
-            <li class="mb-2"><a href="https://sgbau.ac.in/pages/EmploymentBulletin.aspx" target = "__blank">Career/Employment Bulletin(SGBAU)</a></li>
-          </ul>
-          </div>
-          <div class="col-6 col-lg-2 mb-3">
-          <h5>...</h5>
-          <hr>
-          <ul class="list-unstyled">
-            <li class="mb-2"><a href="gate.html">GATE</a></li>
-            <li class="mb-2"><a href="opac.html">OPAC</a></li>
-            <li class="mb-2"><a href="facultypublication.html">Faculty Publications</a></li>
-            <li class="mb-2"><a href="newsclips.html">Newspaper clippings</a></li>
-            <li class="mb-2"><a href="rulesandreg.html">Rules and Regulations</a></li>
-            <li class="mb-2"><a href="refmansoft.html">Reference Management Softwares</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-lg-2 mb-3">
+          <li class="mb-2"><a href="index.html">HOME</a></li>
+          <li class="mb-2"><a href="qrcodes.html" target="__blank">QRCodes</a></li>
+          <li class="mb-2"><a href="downloads.html" target="__blank">Downloads</a></li>
+          <li class="mb-2"><a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSckVzdg3A8t4fmZdP33tw8dr4q5Nr8G76GyOYCLaxzPsYrvXA/viewform"
+              target="__blank">SGBAU PYQs</a></li>
+          <li class="mb-2"><a href="events.html" target="__blank">Events and Activities</a></li>
+          <li class="mb-2"><a href="https://sgbau.ac.in/pages/EmploymentBulletin.aspx"
+              target="__blank">Career/Employment Bulletin(SGBAU)</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-lg-2 mb-3">
         <h5>...</h5>
         <hr>
-          <ul class="list-unstyled">
+        <ul class="list-unstyled">
+          <li class="mb-2"><a href="gate.html">GATE</a></li>
+          <li class="mb-2"><a href="opac.html">OPAC</a></li>
+          <li class="mb-2"><a href="facultypublication.html">Faculty Publications</a></li>
+          <li class="mb-2"><a href="newsclips.html">Newspaper clippings</a></li>
+          <li class="mb-2"><a href="rulesandreg.html">Rules and Regulations</a></li>
+          <li class="mb-2"><a href="refmansoft.html">Reference Management Softwares</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-lg-2 mb-3">
+        <h5>...</h5>
+        <hr>
+        <ul class="list-unstyled">
           <li class="mb-2"><a href="events.html">Events</a></li>
           <li class="mb-2"><a href="delnet.html">DELNET</a></li>
-            <li class="mb-2"><a href="genServices.html">General Services</a></li>
-            <li class="mb-2"><a href="docdelser.html">Document Delivery Services</a></li>
-            <li class="mb-2"><a href="index.html">Institutional Repository</a></li>
-            <li class="mb-2"><a href="perresguide.html">Personalised Research Id's</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-lg-2 mb-3">
-          <h5>...</h5>
-          <hr>
-          <ul class="list-unstyled">
+          <li class="mb-2"><a href="genServices.html">General Services</a></li>
+          <li class="mb-2"><a href="docdelser.html">Document Delivery Services</a></li>
+          <li class="mb-2"><a href="index.html">Institutional Repository</a></li>
+          <li class="mb-2"><a href="perresguide.html">Personalised Research Id's</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-lg-2 mb-3">
+        <h5>...</h5>
+        <hr>
+        <ul class="list-unstyled">
           <li class="mb-2"><a href="ndl.html">NDL</a></li>
           <li class="mb-2"><a href="faqs.html">FAQs</a></li>
           <li class="mb-2"><a href="nlist.html">NLIST</a></li>
-            <li class="mb-2"><a href="libraryteam.html">Library Team</a></li>
-            <li class="mb-2"><a href="libcomittee.html">Library Committee</a></li>
-            <li class="mb-2"><a href="ndloic.html">National Digital Library Of India Club</a></li>
-          </ul>
-        </div>
+          <li class="mb-2"><a href="libraryteam.html">Library Team</a></li>
+          <li class="mb-2"><a href="libcomittee.html">Library Committee</a></li>
+          <li class="mb-2"><a href="ndloic.html">National Digital Library Of India Club</a></li>
+        </ul>
       </div>
-      </div>
-      </footer>`
+    </div>
+    <div class="creator" style="font-size: 13px; display: flex; flex-direction: column;">
+      <p>Website created and Managed by:</p>
+      <p>Harshal Dilip Warade</p>
+      <p>3rd Year IT Section-B - LinkedIn -> <a href="https://www.linkedin.com/in/harshalwarade/"> https://www.linkedin.com/in/harshalwarade/</a> </p>
+    </div>
+  </div>
+</footer>`
 
 );
